340939871f5c86e4cb9e042eec2de01e0f1b74eb
Steven DuBois <sdubois@csh.rit.edu>
2011-06-02 18:17:26 -0400

    removed pywapi folder

--- pywapi-0.2.1/examples/pywapi-google-example.py
+++
@@ -1,6 +0,0 @@
-import pywapi
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-result = pywapi.get_weather_from_google('10001')
-pp.pprint(result)
--- pywapi-0.2.1/examples/pywapi-yahoo-example.py
+++
@@ -1,7 +0,0 @@
-import pywapi
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
-result = pywapi.get_weather_from_yahoo('10001', 'metric')
-pp.pprint(result)
-
