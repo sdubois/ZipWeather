6943f6dad537d37e24f006f8401cf7bd6ea1fc3c
Steven DuBois <sdubois@csh.rit.edu>
2011-06-02 22:05:04 -0400

    added try block

--- zipweather.py
+++ zipweather.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 import pywapi
-import pprint
-
-pp = pprint.PrettyPrinter(indent=4)
 
 app = Flask(__name__)
 
@@ -12,10 +9,16 @@ def index():
 
 @app.route('/<zipcode>/')
 def weather(zipcode):
-    result = pywapi.get_weather_from_google(zipcode)
-    cur_temp=result['current_conditions']['temp_f']
-    city=result['forecast_information']['city']
-    return render_template('weather.html', zipcode=zipcode, city=city, cur_temp=cur_temp)
+    valid = True
+    try:
+        result = pywapi.get_weather_from_google(zipcode)
+        cur_temp=result['current_conditions']['temp_f']
+        city=result['forecast_information']['city']
+        return render_template('weather.html', zipcode=zipcode, city=city, cur_temp=cur_temp, valid=valid)
+    except IndexError:
+        valid = False
+        return render_template('weather.html', zipcode=zipcode)
+   
 
 if __name__ == '__main__':
     app.debug = True
