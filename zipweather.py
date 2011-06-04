eeb21504c56741f7afabc2b8be46fc42a10bb857
Steven DuBois <sdubois@csh.rit.edu>
2011-06-03 21:32:46 -0400

    main functionality in place

--- zipweather.py
+++ zipweather.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, session, g, redirect, \
+url_for, abort, render_template, flash
 import pywapi
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        zipcode = request.form['zipcode']
+        return redirect(url_for('weather', zipcode=zipcode))
     return render_template('index.html')
 
 @app.route('/<zipcode>/')
 def weather(zipcode):
     valid = True
     try:
-        result = pywapi.get_weather_from_google(zipcode)
-        cur_temp=result['current_conditions']['temp_f']
-        city=result['forecast_information']['city']
-        return render_template('weather.html', zipcode=zipcode, city=city, cur_temp=cur_temp, valid=valid)
+        result = pywapi.get_weather_from_google(zipcode)    
+        return render_template('weather.html', \
+        zipcode=zipcode, valid=valid, weather=result)
     except IndexError:
         valid = False
-        return render_template('weather.html', zipcode=zipcode)
+        return render_template('weather.html', \
+        zipcode=zipcode, valid=valid)
    
 
 if __name__ == '__main__':
