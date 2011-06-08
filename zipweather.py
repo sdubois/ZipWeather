6b35655eced860cb8f446d51c10df7a3e6461e48
Steven DuBois <sdubois@csh.rit.edu>
2011-06-08 16:05:23 -0400

    added weather icons

--- zipweather.py
+++ zipweather.py
@@ -18,19 +18,64 @@ def index():
 
 @app.route('/<zipcode>/')
 def weather(zipcode):
-    result = zipcode.isdigit()
-    if result:
+    boolzip = zipcode.isdigit()
+    if boolzip:
         valid = True
     else:
         valid = False
     try:
-        result = pywapi.get_weather_from_google(zipcode)    
+        result = pywapi.get_weather_from_google(zipcode)
+        condition = result['current_conditions']['condition']
+        if condition == 'Sunny' or condition == 'Clear':
+            image = 'Sunny.png'
+        if condition == 'Thunderstorm' or \
+           condition == 'Chance of TStorm' or \
+           condition == 'Scattered Thunderstorms' or\
+           condition == 'Chance of Storm' or\
+           condition == 'Rain':
+            image = 'Thunderstorm.png'
+        if condition == 'Storm' or \
+           condition == 'Showers' or \
+           condition == 'Scattered Showers' or \
+           condition == 'Chance of Rain':
+            image = 'Rain.png'
+        if condition == 'Light Rain':
+            image = 'RainLight.png'
+        if condition == 'Rain and Snow':
+            image = 'RainSnow.png'
+        if condition == 'Snow Showers' or \
+           condition == 'Chance of Snow' or \
+           condition == 'Snow':
+            image = 'Snow.png'
+        if condition == 'Light Snow' or condition == 'Flurries':
+            image = 'SnowLight.png'
+        if condition == 'Hail':
+            image = 'Hail.png'
+        if condition == 'Freezing Drizzle':
+            image = 'FreezingDrizzle.png'
+        if condition == 'Partly Sunny' or condition == 'Mostly Cloudy':
+            image = 'MostlyCloud.png'
+        if condition == 'Mostly Sunny' or condition == 'Partly Cloudy':
+            image = 'MostlySunny.png'
+        if condition == 'Cloudy' or condition == 'Overcast':
+            image = 'Cloudy.png'
+        if condition == 'Sleet' or condition == 'Icy':
+            image = 'Sleet.png'
+        if condition == 'Mist':
+            image = 'Mist.png'
+        if condition == 'Fog':
+            image = 'Fog.png'
+        if condition == 'Smoke':
+            image = 'Smoke.png'
+        if condition == 'Dust' or condition == 'Haze':
+            image = 'Dust.png'
+        image = "../static/images/weather/"+image
         return render_template('weather.html', \
-        zipcode=zipcode, valid=valid, weather=result)
+        zipcode=zipcode, valid=valid, weather=result,  image=image)
     except IndexError:
         valid = False
         return render_template('weather.html', \
-        zipcode=zipcode, valid=valid)
+        zipcode=zipcode, valid=valid,)
    
 
 if __name__ == '__main__':
